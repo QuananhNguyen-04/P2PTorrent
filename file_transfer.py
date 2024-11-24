d5d905a1d967f8f7cb6ce28b29d1dc7e98c8cd33
QuananhNguyen-04 <sherlingfordk@gmail.com>
2024-11-24 18:46:34 +0700

    Add send_file(), send_torrent()

--- file_transfer.py
+++ file_transfer.py
@@ -1,11 +1,14 @@
 import socket
 from threading import Thread
 import os
-from torrent import *
-SERVER_HOST = '192.168.43.7'  # Listen on all available interfaces
+from struct import pack
+from torrent import generate_handshake
+
+SERVER_HOST = "192.168.43.7"  # Listen on all available interfaces
 SERVER_PORT = 681
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 16384  # 16KB
 SEPARATOR = "<SEPARATOR>"
+
 
 def handle_client(client_socket: socket.socket):
     print("handle_client")
@@ -13,7 +16,7 @@ def handle_client(client_socket: socket.socket):
     print(received)
     if SEPARATOR in received:
         command, filename, info_hash, peer_id = received.split(SEPARATOR)
-        
+
         handshake = generate_handshake(info_hash, peer_id)
         client_socket.send(handshake.encode())
 
@@ -22,49 +25,103 @@ def handle_client(client_socket: socket.socket):
             print("Handshake failed")
             client_socket.close()
             return
-        
+
         if command == "UPLOAD":
             receive_file(client_socket, filename)
         elif command == "DOWNLOAD":
             send_file(client_socket, filename)
     else:
         print("Invalid command received")
-    
+
     client_socket.close()
+
 
 def receive_file(client_socket: socket.socket, filename: str):
     with open(filename, "wb") as f:
         while True:
             bytes_read = client_socket.recv(BUFFER_SIZE)
-            if not bytes_read:    
+            if not bytes_read:
                 break
             f.write(bytes_read)
     print(f"Received file: {filename}")
 
-def send_file(client_socket: socket.socket, filename):
+
+def send_file(
+    client_socket: socket.socket,
+    filename: str,
+    piece_index: int,
+    begin_offset: int,
+    piece_length: int,
+):
+    print(filename)
     if not os.path.exists(filename):
         client_socket.send("File not found".encode())
         return
 
-    with open(filename, "rb") as f:
-        while True:
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-                break
-            client_socket.sendall(bytes_read)
-    print(f"Sent file: {filename}")
+    # Calculate the starting position within the file
+    start_position = piece_index * piece_length + begin_offset
+    end_position = (piece_index + 1) * piece_length
+    try:
+        with open(filename, "rb") as f:
+            # Move to the start position of the requested piece
+            f.seek(start_position)
+            # Send the requested piece data only
+            bytes_to_read = min(BUFFER_SIZE, end_position - start_position)
+            bytes_read = f.read(bytes_to_read)
+            print(start_position, end_position, len(bytes_read))
+            new_offset = begin_offset + len(bytes_read)
+            print(new_offset)
+            message = (
+                pack(
+                    ">IBIII",
+                    13 + len(bytes_read),
+                    7,
+                    piece_index,
+                    new_offset,
+                    len(bytes_read),
+                )
+                + bytes_read
+            )
+            # print(message)
+            client_socket.sendall(message)
+
+        print(
+            f"Sent {piece_length} bytes from file: {filename}, starting at offset {start_position}"
+        )
+
+    except Exception as e:
+        print(f"Error sending file piece: {e}")
+        client_socket.send("Error sending file piece".encode())
+
+
+def send_torrent(client_socket: socket.socket, filename_raw: str):
+    try:
+        with open(f"{filename_raw+ '.torrent'}", "rb") as f:
+            torrent_data = f.read()
+
+            message = (
+                pack(">IBIII", 13 + len(torrent_data), 5, 0, 0, len(torrent_data))
+                + torrent_data
+            )
+            client_socket.sendall(message)
+            print(f"Sent torrent file: {filename_raw + '.torrent'}")
+    except Exception as e:
+        print(f"Error sending torrent file: {e}")
+        client_socket.send("Error sending torrent file".encode())
+
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((SERVER_HOST, SERVER_PORT))
     server_socket.listen(5)
     print(f"Listening on {SERVER_HOST}:{SERVER_PORT}...")
-    
+
     while True:
         client_socket, address = server_socket.accept()
         print(f"Accepted connection from {address}")
         client_handler = Thread(target=handle_client, args=(client_socket,))
         client_handler.start()
+
 
 if __name__ == "__main__":
     start_server()
